31599c83f0d604b6e3ff1584dc55377a5ab1e564
M.Sattar <106179236+MSpider5@users.noreply.github.com>
2022-09-24 08:31:42 +0400

    Pause menu, Loading screen added, localization not finished

--- RpgMinigame/Assets/App/Scripts/Menu_Scripts/MainMenu.cs
+++ RpgMinigame/Assets/App/Scripts/Menu_Scripts/MainMenu.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public string firstStart;
+
+    public GameObject loadingScreen, loadingIcon;
+    public Text loadingText;
     public void OpenGame()
     {
-        SceneManager.LoadScene(firstStart);
+        //SceneManager.LoadScene(firstStart);
+        StartCoroutine(LoadStart());
     }
 
 
@@ -16,6 +21,33 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    public IEnumerator LoadStart()
+    {
+        loadingScreen.SetActive(true);
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(firstStart);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            if (asyncLoad.progress >= .9f)
+            {
+                loadingText.text = "Press any key to continue";
+                loadingIcon.SetActive(false);
+
+                if (Input.anyKeyDown)
+                {
+                    asyncLoad.allowSceneActivation = true;
+
+                    //Time.timeScale = 1f;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
 
 
 }
